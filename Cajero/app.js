d77bee30dde0520a00176920f6e3356f78fb15f0
luzsdiaz <bobiaz@hotmail.com>
2022-05-24 06:38:23 -0500

    Final Cajero

--- Cajero/app.js
+++ Cajero/app.js
@@ -16,12 +16,12 @@ function cardcheck(){
     var textv=document.getElementById('textview').value;
 
     if(textv==""){
-        document.getElementById('msg').innerHTML="Numero de cliento no ingresado";
+        document.getElementById('msg').innerHTML="Numero de cliente no ingresado";
     }
     else{
-        if(textv=1111)
+        if(textv==1111)
         {
-            document.getElementById('msg').style.color="blue";
+            document.getElementById('msg').style.color="green";
             document.getElementById('msg2').innerHTML="Numero de cuenta correcto";
             document.getElementById('msg').innerHTML="Ingresa tu contraseña";
             document.getElementById('textview').value="";
@@ -45,7 +45,7 @@ function passcheck(){
     else{
         if(textv==1234){
         document.getElementById('msg').style.color="blue";
-        document.getElementById('msg2').innerHTML="contraseña correcta";
+        document.getElementById('msg2').innerHTML="CONTRASEÑA CORRECTA";
         document.getElementById('msg').innerHTML="Elija una opcion";
         document.getElementById('textview').value="";
         document.getElementById('menu').style.display="block";
@@ -54,7 +54,7 @@ function passcheck(){
         }
         else{
             document.getElementById('msg2').innerHTML="";
-            document.getElementById('msg').innerHTML="contraseña icorrecta";
+            document.getElementById('msg').innerHTML="contraseña incorrecta";
 
         }
     }
@@ -64,6 +64,17 @@ function passcheck(){
 
 
 //Funcion para hace rel calculo cuando se retira dinero
+
+function withdraw() {
+    document.getElementById('msg').innerHTML="Ingresa la cantidad a retirar"
+    document.getElementById('menu').style.display="none"
+    document.getElementById('menuback').style.display="block"
+    document.getElementById('btnproced').setAttribute("onclick","calculation1()")
+    document.getElementById('textview').style.display="block"
+}
+
+
+
 function calculation1() {
     var textview=document.getElementById('textview').value;
     var textv=parseInt(textview);
@@ -73,14 +84,14 @@ function calculation1() {
             document.getElementById('msg').innerHTML=""; 
         }
     else{
-        if(totalAmmount>1 && textv<totalAmmount){
+        if(totalAmmount>10 && textv<totalAmmount){
             totalAmmount=totalAmmount-textv;
             document.getElementById('msg2').innerHTML="Tu saldo es: ";
             document.getElementById('msg').innerHTML=totalAmmount;
             document.getElementById('textview').value="";
         }
         else{
-            document.getElementById('msg').innerHTML="Saldo insuficiente";
+            document.getElementById('msg').innerHTML="SALDO INSUFICIENTE";
         }
     }
 };
@@ -99,20 +110,25 @@ function credit() {
 
 
 
-
-
 function calculation2() {
     var textv=document.getElementById('textview').value;
     if(textv=="" || textv==0)
-    {
+        {
         document.getElementById('msg2')="No se ingreso ningun monto";
         document.getElementById('msg').innerHTML=""; 
-    }
+        }
     else{
+        if(textv>980 || totalAmmount>=990){
+            document.getElementById('msg2').innerHTML="Tu cuenta no debe de tener más de $990";
+            document.getElementById('msg').innerHTML=totalAmmount;
+            document.getElementById('textview').value="";
+        }
+        else{
         totalAmmount=totalAmmount+parseFloat(textv);
         document.getElementById('msg2').innerHTML="Tu saldo es: ";
         document.getElementById('msg').innerHTML=totalAmmount;
         document.getElementById('textview').value="";   
+    }
     }
 };
 
@@ -122,7 +138,7 @@ function menuback(){
     document.getElementById('menuback').style.display="none";
     document.getElementById('btnproced').setAttribute("onclick","");
     document.getElementById('consult2').style.display="none";
-    document.getElementById('msg').innerHTML="choose option";
+    document.getElementById('msg').innerHTML="SELECCIONA UNA OPCION";
     document.getElementById('msg2').innerHTML="";
 
 };
@@ -133,7 +149,7 @@ function consult(){
     var textv=document.getElementById('textview').value;
     document.getElementById('consult2').style.display="block";
     document.getElementById('textview').style.display="none";
-    document.getElementById('msg').innerHTML="Tu credito es de:-";
+    document.getElementById('msg').innerHTML="";
     document.getElementById('msg2').innerHTML="";
     document.getElementById('main-balance').innerHTML=totalAmmount;
     document.getElementById('menuback').style.display="block";
